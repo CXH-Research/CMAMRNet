18518d97cec38f36bb4afd6e81309fe690030208
xuhangc <cxhhg11@gmail.com>
2024-11-07 12:21:10 +0800

    pipeline

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ import warnings
 
 import torch.optim as optim
 from accelerate import Accelerator
-from pytorch_msssim import SSIM
 from torch.utils.data import DataLoader
 from torchmetrics.functional import peak_signal_noise_ratio, structural_similarity_index_measure
 from tqdm import tqdm
@@ -25,7 +24,7 @@ def train():
     config = {
         "dataset": opt.TRAINING.TRAIN_DIR
     }
-    accelerator.init_trackers("shadow", config=config)
+    accelerator.init_trackers("mural", config=config)
 
     if accelerator.is_local_main_process:
         os.makedirs(opt.TRAINING.SAVE_DIR, exist_ok=True)
@@ -34,18 +33,14 @@ def train():
     train_dir = opt.TRAINING.TRAIN_DIR
     val_dir = opt.TRAINING.VAL_DIR
 
-    train_dataset = get_training_data(train_dir, opt.MODEL.INPUT, opt.MODEL.TARGET, {'w': opt.TRAINING.PS_W, 'h': opt.TRAINING.PS_H})
-    trainloader = DataLoader(dataset=train_dataset, batch_size=opt.OPTIM.BATCH_SIZE, shuffle=True, num_workers=16,
-                             drop_last=False, pin_memory=True)
-    val_dataset = get_validation_data(val_dir, opt.MODEL.INPUT, opt.MODEL.TARGET, {'w': opt.TRAINING.PS_W, 'h': opt.TRAINING.PS_H, 'ori': opt.TRAINING.ORI})
-    testloader = DataLoader(dataset=val_dataset, batch_size=1, shuffle=False, num_workers=8, drop_last=False,
-                            pin_memory=True)
+    train_dataset = get_training_data(train_dir, {'w': opt.TRAINING.PS_W, 'h': opt.TRAINING.PS_H})
+    trainloader = DataLoader(dataset=train_dataset, batch_size=opt.OPTIM.BATCH_SIZE, shuffle=True, num_workers=16, drop_last=False, pin_memory=True)
+    val_dataset = get_validation_data(val_dir, {'w': opt.TRAINING.PS_W, 'h': opt.TRAINING.PS_H, 'ori': opt.TRAINING.ORI})
+    testloader = DataLoader(dataset=val_dataset, batch_size=1, shuffle=False, num_workers=8, drop_last=False, pin_memory=True)
 
     # Model & Loss
     model = Model()
-    criterion_ssim = SSIM(data_range=1, size_average=True, channel=3).to(device)
     criterion_psnr = torch.nn.MSELoss()
-    criterion_bce = torch.nn.BCELoss()
 
     # Optimizer & Scheduler
     optimizer_b = optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=opt.OPTIM.LR_INITIAL, betas=(0.9, 0.999), eps=1e-8)
@@ -71,13 +66,12 @@ def train():
 
             # forward
             optimizer_b.zero_grad()
-            res, pred = model(inp)
+            res = model(inp)
 
             loss_psnr = criterion_psnr(res, tar)
-            loss_ssim = 1 - criterion_ssim(res, tar)
-            loss_mask = criterion_bce(pred, mas).mean()
+            loss_ssim = 1 - structural_similarity_index_measure(res, tar, data_range=1)
 
-            train_loss = loss_psnr + 0.4 * loss_ssim + loss_mask
+            train_loss = loss_psnr + 0.4 * loss_ssim
 
             # backward
             accelerator.backward(train_loss)
@@ -97,7 +91,7 @@ def train():
                 tar = data[2]
 
                 with torch.no_grad():
-                    res, _ = model(inp)
+                    res = model(inp)
 
                 res, tar = accelerator.gather((res, tar))
 
