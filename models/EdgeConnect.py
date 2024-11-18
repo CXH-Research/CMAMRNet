35b7d27f4885d6af5befaea8a1b787c81066e080
Nick Chen <cxhhg11@gmail.com>
2024-11-18 17:46:53 +0800

    Add files via upload

---
+++ models/EdgeConnect.py
@@ -0,0 +1,124 @@
+import torch
+import torch.nn as nn
+
+
+def spectral_norm(module, mode=True):
+    if mode:
+        return nn.utils.spectral_norm(module)
+    return module
+
+
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, dilation=1, use_spectral_norm=False):
+        super(ResnetBlock, self).__init__()
+        self.conv_block = nn.Sequential(
+            nn.ReflectionPad2d(dilation),
+            spectral_norm(nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=3,
+                          padding=0, dilation=dilation, bias=not use_spectral_norm), use_spectral_norm),
+            nn.InstanceNorm2d(dim, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.ReflectionPad2d(1),
+            spectral_norm(nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=3,
+                          padding=0, dilation=1, bias=not use_spectral_norm), use_spectral_norm),
+            nn.InstanceNorm2d(dim, track_running_stats=False),
+        )
+
+    def forward(self, x):
+        out = x + self.conv_block(x)
+        return out
+
+
+class EdgeConnect(nn.Module):
+    def __init__(self, residual_blocks=8, init_weights=True):
+        super(EdgeConnect, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_channels=4, out_channels=64,
+                      kernel_size=7, padding=0),
+            nn.InstanceNorm2d(64, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.Conv2d(in_channels=64, out_channels=128,
+                      kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(128, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.Conv2d(in_channels=128, out_channels=256,
+                      kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(256, track_running_stats=False),
+            nn.ReLU(True)
+        )
+
+        blocks = []
+        for _ in range(residual_blocks):
+            block = ResnetBlock(256, 2)
+            blocks.append(block)
+
+        self.middle = nn.Sequential(*blocks)
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=256, out_channels=128,
+                               kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(128, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(in_channels=128, out_channels=64,
+                               kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(64, track_running_stats=False),
+            nn.ReLU(True),
+
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_channels=64, out_channels=3,
+                      kernel_size=7, padding=0),
+        )
+
+        if init_weights:
+            self.init_weights()
+
+
+    def init_weights(self, init_type='normal', gain=0.02):
+        '''
+        initialize network's weights
+        init_type: normal | xavier | kaiming | orthogonal
+        https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/9451e70673400885567d08a9e97ade2524c700d0/models/networks.py#L39
+        '''
+
+        def init_func(m):
+            classname = m.__class__.__name__
+            if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+                if init_type == 'normal':
+                    nn.init.normal_(m.weight.data, 0.0, gain)
+                elif init_type == 'xavier':
+                    nn.init.xavier_normal_(m.weight.data, gain=gain)
+                elif init_type == 'kaiming':
+                    nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+                elif init_type == 'orthogonal':
+                    nn.init.orthogonal_(m.weight.data, gain=gain)
+
+                if hasattr(m, 'bias') and m.bias is not None:
+                    nn.init.constant_(m.bias.data, 0.0)
+
+            elif classname.find('BatchNorm2d') != -1:
+                nn.init.normal_(m.weight.data, 1.0, gain)
+                nn.init.constant_(m.bias.data, 0.0)
+
+        self.apply(init_func)
+
+
+    def forward(self, x, mask):
+        x = torch.cat([x, mask], dim=1)
+        x = self.encoder(x)
+        x = self.middle(x)
+        x = self.decoder(x)
+        x = (torch.tanh(x) + 1) / 2
+        return x
+
+
+if __name__ == "__main__":
+    model = EdgeConnect()
+    x = torch.randn(1, 3, 256, 256)
+    mask = torch.randn(1, 1, 256, 256)
+    y = model(x, mask)
+    print(y.shape)
